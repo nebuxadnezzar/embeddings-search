1a9202d230bb864714dad65fe09a87c973a041ba
nebuxadnezzar <nebuxadnezzar@aol.com>
2023-08-14 19:33:38 -0400

    modified json_to_prefix

--- json_to_prefix.py
+++ json_to_prefix.py
@@ -3,7 +3,7 @@
 #
 #  json_to_prefix.py
 #
-
+import re
 import sys
 import json
 import fileinput
@@ -15,9 +15,14 @@ if sys.version_info[0] > 2:
 
 parser = argparse.ArgumentParser()
 parser.add_argument( '-out', dest='pfxFile', help='prefixes file', required=True, metavar="FILE" )
-parser.add_argument( '-input', dest='jsonFile', help='prefixes file', required=True, metavar="FILE" )
+parser.add_argument( '-input', dest='jsonFile', help='json file', required=True, metavar="FILE" )
+parser.add_argument( '-idfield', dest='id', type=str, default="", help='id field', metavar="SYMBOL" )
 
 locale.setlocale( locale.LC_ALL, '')
+
+ns_rx = re.compile(r'[{]([^{}]+)[}]')
+pu_rx = re.compile(r'[^\w\s]')
+stop_words_rx = re.compile(r'\s+(and|y|the|a|d|s|ll|re|ve|your|yours)\s+')
 
 testjs = \
 '''
@@ -30,6 +35,11 @@ testjs = \
 '''
 
 #==============================================================================
+def tokenize(vals):
+    return re.sub('\s+', ' ', re.sub(stop_words_rx, ' ', re.sub(pu_rx, ' ', vals))).strip().split()
+    #return re.sub('\s+', ' ',re.sub(pu_rx, ' ', vals)).strip().split()
+
+#==============================================================================
 def updatePfxMap(map, key, id):
     if key in map:
         map[key].append(id)
@@ -38,40 +48,68 @@ def updatePfxMap(map, key, id):
     return map
 #==============================================================================
 def jsonToPfxLst(pfx, pfxmap, id, lst):
-    print(lst)
+    # print(lst)
 
     for i in lst:
+        #print(f'++> {i}')
+        if not i: continue
         if isinstance(i, dict):
-            jsonToPfx(f'{pfx}:', pfxmap, id, i)
+            jsonToPfx(f'{pfx}', pfxmap, id, i)
         elif isinstance(i, list):
             jsonToPfxLst(f'{pfx}:', pfxmap, id, i)
         else:
-            key = f'{pfx}{i}'
-            updatePfxMap(pfxmap, key, id)
+            for val in tokenize(i):
+                key = f'{pfx}{val}'
+                # pfxmap['_keys_'].add(pfx)
+                updatePfxMap(pfxmap, key, id)
 
 #==============================================================================
 def jsonToPfx(pfx, pfxmap, id, doc):
-    print(doc)
+    # print(doc)
 
     for k in doc:
-        print(f'--> {k}')
+        #print(f'--> {k}')
         if isinstance(doc[k], dict):
             jsonToPfx(f'{pfx}{k}:', pfxmap, id, doc[k])
         elif isinstance(doc[k], list):
+            pfxmap['_keys_'].add(f'{pfx}{k}')
             jsonToPfxLst(f'{pfx}{k}:', pfxmap, id, doc[k])
         else:
-            key = f'{pfx}{k}:{doc[k]}'
-            updatePfxMap(pfxmap, key, id)
+            for val in tokenize(doc[k]):
+                key = f'{pfx}{k}:{val}'
+                pfxmap['_keys_'].add(f'{pfx}{k}')
+                updatePfxMap(pfxmap, key, id)
 
 #==============================================================================
-def main(args):
-    print(args)
-    # opts = parser.parse_args()
-    pfxmap = {}
-    print(testjs)
-    jsonToPfx("", pfxmap, 0, json.loads(testjs))
+def pfxToFile(path, records):
+    f = open(path, 'w')
 
-    print(json.dumps(pfxmap, indent=1))
+    for rec in records:
+        # print(f'--> {rec}')
+        f.write(json.dumps(rec))
+        f.write("\n")
+    f.flush()
+    f.close()
+#==============================================================================
+def main(args):
+    # print(testjs)
+    opts = parser.parse_args()
+    pfxmap = {'_keys_':set([])}
+    # jsonToPfx("", pfxmap, 0, json.loads(testjs))
+
+    cnt = 0
+    for line in fileinput.input(opts.jsonFile):
+        o = json.loads(line)
+        if not opts.id:
+            jsonToPfx("", pfxmap, cnt, o)
+        else:
+            jsonToPfx("", pfxmap, o[opts.id] if opts.id in o else cnt, o)
+        cnt += 1
+        sys.stderr.write( f"\rrecord count: {cnt}")
+    sys.stderr.write( "\n")
+    pfxmap['_keys_'] = list(pfxmap['_keys_'])
+    pfxToFile(opts.pfxFile, pfxmap.items())
+    #print(json.dumps(pfxmap, indent=1))
 
     return 0
 
@@ -79,4 +117,9 @@ if __name__ == '__main__':
     sys.exit(main(sys.argv))
 
 
+'''
+json_to_prefix.py -in data/sdn.json -o ~/ephemeral/sdn_pfx.txt
+json_to_prefix.py -in data/sdn.json -o ~/ephemeral/sdn_pfx.txt -id "_id"
+json_to_prefix.py -in data/sdn.json -o ~/ephemeral/sdn_pfx.txt -id "missing-id"
+'''
 
